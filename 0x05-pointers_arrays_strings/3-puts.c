be6a75b68656c2294de4e7cf2b6ad5d709244662
Cyguy1999 <Cyguy7@gmail.com>
2022-09-28 10:23:47 -0700

    print a sring with a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * *_puts - Entry function to print string
- * *@str: pointer to value
- * *Description: prints string
- * *Return: 0 = Success
- * */
+*_puts - Entry function to print string
+*@str: pointer to value
+*Description: prints string
+*Return: 0 = Success
+*/
 void _puts(char *str)
 {
 	while (*str)
